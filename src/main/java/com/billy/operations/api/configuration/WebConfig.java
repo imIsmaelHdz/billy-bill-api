5582a3f36927730c3e41ea15841be920775cb44f
IsmaelHdz <imismaelhdz@gmail.com>
2023-11-16 23:40:37 -0600

    Java objects for login configuration

--- src/main/java/com/billy/operations/api/configuration/WebConfig.java
+++ src/main/java/com/billy/operations/api/configuration/WebConfig.java
@@ -3,17 +3,14 @@ package com.billy.operations.api.configuration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig {
-
+    private static final Long MAX_AGE = 3600L;
+    private static final int CORS_FILTER_ORDER = -102;
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {return null;}
 }
